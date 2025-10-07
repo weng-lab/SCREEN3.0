f51286176d006a0e852c96f797614c422c076aae
mlacadie <lacadiem14@gmail.com>
2025-10-07 15:21:59 -0400

    fix #126

--- src/app/[assembly]/[entityType]/[entityID]/[[...tab]]/page.tsx
+++ src/app/[assembly]/[entityType]/[entityID]/[[...tab]]/page.tsx
@@ -4,7 +4,6 @@ import GenomeBrowserView from "common/gbview/genomebrowserview";
 import { useEntityMetadata, useEntityMetadataReturn } from "common/hooks/useEntityMetadata";
 import { isValidAssembly } from "types/globalTypes";
 import {
-  entityTabsConfig,
   getComponentForEntity,
   isValidEntityType,
   isValidRouteForEntity,
@@ -25,12 +24,10 @@ import { GWASStudyGenes } from "./_GwasTabs/_Gene/GWASStudyGenes";
 import { GWASStudySNPs } from "./_GwasTabs/_Variant/GWASStudySNPs";
 import BiosampleEnrichment from "./_GwasTabs/_BiosampleEnrichment/BiosampleEnrichment";
 import {
-  AnyOpenEntity,
   CandidateOpenEntity,
   isValidOpenEntity,
 } from "common/EntityDetails/OpenEntitiesTabs/OpenEntitiesContext";
 import GWASGenomeBrowserView from "./_GwasTabs/_Browser/gwasgenomebrowserview";
-import { useGWASSnpsData } from "common/hooks/useGWASSnpsData";
 import VariantLinkedCcres from "./_SnpTabs/_cCREs/VariantLinkedCcres";
 import TranscriptExpression from "./_GeneTabs/_Transcript/TranscriptExpression";
 
