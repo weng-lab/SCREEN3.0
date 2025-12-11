e61a11508218066e988a86f49fbe0536504515a5
NishiPhalke <nishaphalke@gmail.com>
2025-12-11 11:52:17 -0500

    added computational linked genes for a cCRE

--- src/common/components/linkedElements.tsx
+++ src/common/components/linkedElements.tsx
@@ -4,13 +4,14 @@ import { LinkedGeneInfo } from "common/hooks/useLinkedGenes";
 import { useLinkedCcresReturn } from "common/hooks/useLinkedCcres";
 import { Table, TableProps } from "@weng-lab/ui-components";
 import { useCompuLinkedcCREsReturn } from "common/hooks/useCompuLinkedcCREs";
+import { useCompuLinkedGenesReturn } from "common/hooks/useCompuLinkedGenes";
 
-export type TableDef<T extends LinkedGeneInfo | useLinkedCcresReturn["data"][number] | useCompuLinkedcCREsReturn["data"][number]> = TableProps & {
+export type TableDef<T extends LinkedGeneInfo | useCompuLinkedGenesReturn["data"][number] | useLinkedCcresReturn["data"][number] | useCompuLinkedcCREsReturn["data"][number]> = TableProps & {
   sortColumn: keyof T & string; // Constrain to string keys
   sortDirection: "asc" | "desc";
 };
 
-export default function LinkedElements<T extends LinkedGeneInfo | useLinkedCcresReturn["data"][number] | useCompuLinkedcCREsReturn["data"][number]>({
+export default function LinkedElements<T extends LinkedGeneInfo | useCompuLinkedGenesReturn["data"][number] | useLinkedCcresReturn["data"][number] | useCompuLinkedcCREsReturn["data"][number]>({
   tables,
 }: {
   tables: TableDef<T>[];
