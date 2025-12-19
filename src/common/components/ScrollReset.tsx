97d25b050912d236d89d8e373a52ab133884199d
mlacadie <lacadiem14@gmail.com>
2025-12-19 10:01:22 -0500

    Update ScrollReset.tsx

--- src/common/components/ScrollReset.tsx
+++ src/common/components/ScrollReset.tsx
@@ -6,8 +6,6 @@ export default function ScrollReset() {
   const pathname = usePathname();
 
   useEffect(() => {
-    // Manually scroll to top on page change
-    if (pathname.startsWith("/about")) return; // ignore about page
     const el = document.getElementById("content-wrapper");
     if (el) el.scrollTo(0, 0);
   }, [pathname]);
