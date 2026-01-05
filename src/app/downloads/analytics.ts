9b4c78b43099ae364c3707006aa35d67a8d9cb6e
Jair Meza <jair3202@gmail.com>
2026-01-05 13:54:19 -0500

    added console log for testing

--- src/app/downloads/analytics.ts
+++ src/app/downloads/analytics.ts
@@ -1,5 +1,6 @@
 import { track } from "@vercel/analytics";
 
 export function trackDownload(url: string, name: string, source: string) {
+  console.log("file-download", { url, name, source });
   track("file-download", { url, name, source });
 }
