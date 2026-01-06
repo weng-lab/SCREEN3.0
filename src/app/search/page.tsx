93be8e6a212f83ef9d109b489cf4e8fd745a1908
jpfisher72 <jpfisher72@gmail.com>
2026-01-06 15:12:02 -0800

    Changes to v2 results list

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -14,7 +14,7 @@ const ReturnEl = ({ result, assembly }: { result: Result; assembly: Assembly }) 
   return (
     <span>
       <LinkComponent href={url} sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}>
-        <Typography>{result.type === "Gene" ? <i>{result.title}</i> : result.title}</Typography>
+        {result.type === "Gene" ? <i>{result.title}</i> : result.title}
       </LinkComponent>
       {result.type !== "Coordinate" && (
         <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>
@@ -93,15 +93,15 @@ export default function Page({
   const { data: V2Data, loading: V2Loading, error: V2Error } = useV2Ccres(oldScreenAccessions.split(","), assembly);
 
   return (
-    <Stack spacing={2} margin={{ xs: 2, lg: 3, xl: 4 }}>
+    <Stack spacing={2} margin={{ xs: 2, lg: 3, xl: 4 }} maxWidth={800}>
       <Typography variant="h4">{`Results matching "${searchStrings.join(" ")}"`}</Typography>
-      <Typography variant="body2" maxWidth={700}>
+      <Typography variant="body2">
         If you are coming from the ENCODE portal, please find results related to your query below. If you are revising
         this link saved from a previous version of this site, we have attempted to pulled out some items from the URL
         which may be relvant to you.{" "}
         <LinkComponent href={"/about#versions"}>Please see old versions of SCREEN here</LinkComponent>
       </Typography>
-      <Typography variant="body2" maxWidth={700}>
+      <Typography variant="body2">
         Search input is space-separated and terms are searched separately. Results are limited to 10 per result type per
         search term. If you don&apos;t see what you&apos;re looking, please try to search directly on our site.
       </Typography>
@@ -110,57 +110,47 @@ export default function Page({
       {V2Data && V2Data.getv2cCREMappings.length !== 0 && (
         <div>
           <Typography variant="h5">Legacy v2 cCRE</Typography>
-          <Stack alignItems={"flex-start"}>
+          <Stack alignItems={"flex-start"} spacing={1}>
             {V2Data.getv2cCREMappings.map((result, idx) => (
               <div key={idx}>
-                <Typography sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
-                  {result.v2_accession}
+                <span>
                   <Tooltip
-                    placement="right"
                     arrow
-                    title={
-                      <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>
-                        {result.v2_region}
-                      </Typography>
-                    }
-                    key={idx}
+                    placement="right"
+                    title="Opens on old v2 SCREEN site. We recommend using the new site and v4 registry when possible. Previous versions of SCREEN are not actively updated."
                   >
-                    <InfoOutline fontSize="small" />
+                    <LinkComponent
+                      href={`https://screen-v2.wenglab.org/search/?q=${result.v2_accession}&assembly=${assembly}`}
+                      sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
+                      showExternalIcon
+                      openInNewTab
+                    >
+                      {result.v2_accession}
+                    </LinkComponent>
                   </Tooltip>
-                </Typography>
+                  <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>
+                    {result.v2_region}
+                  </Typography>
+                </span>
                 {result.v4_accession ? (
                   <>
-                    <Typography display={"inline"} ml={1}>
-                      {`Corresponding v4 cCRE${result.v4_accession.split(",").length > 1 ? "s" : ""}: `}
+                    <Typography display={"inline"}>
+                      {`Intersecting v4 cCRE${result.v4_accession.split(",").length > 1 ? "s" : ""}: `}
                     </Typography>
                     {result.v4_accession.split(",").map((accession, i, arr) => (
-                      <Tooltip
-                        placement="right"
-                        arrow
-                        title={
-                          <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>
-                            {result.v4_region.split(",")[i]}
-                          </Typography>
-                        }
-                        key={i}
-                      >
-                        <span>
-                          <LinkComponent
-                            href={`/${assembly}/ccre/${accession}`}
-                            sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
-                          >
-                            {accession}
-                            <InfoOutline fontSize="small" />
-                          </LinkComponent>
-                          {!(i === arr.length - 1) && ", "}
-                        </span>
-                      </Tooltip>
+                      <span key={i}>
+                        <LinkComponent
+                          href={`/${assembly}/ccre/${accession}`}
+                          sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
+                        >
+                          {accession}
+                        </LinkComponent>
+                        {!(i === arr.length - 1) && ", "}
+                      </span>
                     ))}
                   </>
                 ) : (
-                  <Typography display={"inline"} ml={1}>
-                    This cCRE has been deprecated
-                  </Typography>
+                  <Typography display={"inline"}>This cCRE has been deprecated in v4 of the cCRE registry</Typography>
                 )}
               </div>
             ))}
