5559c767c6411b5cde905a6c9e485f4a5786820b
jpfisher72 <jpfisher72@gmail.com>
2026-04-10 16:09:46 -0700

    attach api-key in header of requests

--- src/common/apollo/apollo-wrapper.tsx
+++ src/common/apollo/apollo-wrapper.tsx
@@ -8,13 +8,11 @@ import {
   SSRMultipartLink,
   ApolloClient,
 } from "@apollo/experimental-nextjs-app-support";
-import Config from "common/config.json";
-
 // See https://www.apollographql.com/blog/using-apollo-client-with-next-js-13-releasing-an-official-library-to-support-the-app-router
 
 export function makeClient() {
   const httpLink = new HttpLink({
-    uri: Config.API.CcreAPI,
+    uri: "/api/graphql",
   });
 
   return new ApolloClient({
